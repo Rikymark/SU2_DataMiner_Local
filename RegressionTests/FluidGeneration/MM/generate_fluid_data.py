ecbf9e9b36c7f7545b05fab01220ef94f2a3c06e
EvertBunschoten <evertbun@xs4all.nl>
2025-12-12 11:09:43 +0100

    Automatically generate config in regression tests

--- RegressionTests/FluidGeneration/MM/generate_fluid_data.py
+++ RegressionTests/FluidGeneration/MM/generate_fluid_data.py
@@ -5,8 +5,14 @@ import sys
 from su2dataminer.config import Config_NICFD
 from su2dataminer.generate_data import DataGenerator_CoolProp 
 
-Config = Config_NICFD(sys.argv[-1])
+Config = Config_NICFD()
 
+Config.SetFluid("MM")
+Config.SetEquationOfState("HEOS")
+Config.UseAutoRange(True)
+Config.UsePTGrid(False)
+Config.SetNpDensity(50)
+Config.SetNpEnergy(50)
 Config.SetOutputDir(os.getcwd())
 
 D = DataGenerator_CoolProp(Config_in=Config)
