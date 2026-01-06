3fa9c4cba7d2d7d9887224fb0036b9775b9f3b43
EvertBunschoten <evertbun@xs4all.nl>
2026-01-06 11:17:13 +0100

    Included regression test for NICFD PINN training

--- RegressionTests/run_regression.py
+++ RegressionTests/run_regression.py
@@ -30,6 +30,14 @@ def main():
     consistency_EEoS.test_files = ["consistency_check.txt"]
     test_list_NICFD.append(consistency_EEoS)
 
+    consistency_NICFD_PINN = TestCase("PIML training NICFD")
+    consistency_NICFD_PINN.config_dir = "FluidTraining/MM_PINN/"
+    consistency_NICFD_PINN.config_file = ""
+    consistency_NICFD_PINN.exec_command = "./train_MLP.py"
+    consistency_NICFD_PINN.reference_files = ["SU2_MLP_ref.mlp"]
+    consistency_NICFD_PINN.test_files = ["SU2_MLP.mlp"]
+    test_list_NICFD.append(consistency_NICFD_PINN)
+
     hydrogen_flamelet = TestCase("H2_Flamelet")
     hydrogen_flamelet.config_dir = "FlameletGeneration/Adiabatic_H2/"
     hydrogen_flamelet.config_file = "adiabatic_flamelets.cfg"
